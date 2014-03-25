d69475d947e4dfdefeb10d9284626f9a208a17f0
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-03-25 22:54:58 +0000

    Version bump to 1.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(name='i3situation',
       packages=find_packages(),
-      version='1.0.2',
+      version='1.0.3',
       description='A replacement for i3status that allows for the use of plugins.',
       author='Harvey Hunt',
       url='https://github.com/HarveyHunt/i3situation',
